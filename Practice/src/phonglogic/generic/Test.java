f3d2d4a96ad35019891fa8f325798e675afd3616
dangmauquang <dangmauquang149@gmail.com>
2023-05-29 09:14:42 +0700

    ontap-29.05.2023

---
+++ Practice/src/phonglogic/generic/Test.java
@@ -0,0 +1,10 @@
+package phonglogic.generic;
+
+public class Test {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++)
+			for (int j = 2; j <= 20; j++)
+				System.out.println("i=" + i + " j=" + j);
+
+	}
+}
